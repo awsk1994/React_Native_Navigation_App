c267c6ecfa7fca1f0bc3dfa32ca504ea0fec953c
Alex Wong <awsk1994@gmail.com>
2021-01-22 18:01:47 +0800

    Remove fonts-related code. Not working; unsure y

--- App.js
+++ App.js
@@ -2,21 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
+// import * as Font from 'expo-font';
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-};
+// const fetchFonts = () => {
+//   return Font.loadAsync({
+//     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+//     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+//   });
+// };
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
 
-  if(!fontLoaded){
-    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)}/>
-  };
+  // if(!fontLoaded){
+  //   return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)}/>
+  // };
 
   return (
     <View style={styles.container}>
